35b373065028eecbe063ef094775f7b7313e2432
Kenneth Mercer <kmercer5439@gmail.com>
2021-11-30 23:00:34 -0600

    4/10 complete

--- src/index.js
+++ src/index.js
@@ -25,3 +25,38 @@ midImg.addEventListener('dblclick', function (e) {
     midImg.style.border = "";
   }, 500);
 }, false);
+
+//Keydown
+const introp = document.querySelector('.intro p');
+
+introp.addEventListener('keydown', spamKey);
+
+function spamKey(event) {
+    introp.textContent += ` ${event.code}`;
+}
+
+//Wheel
+const midText = document.querySelector('.inverse-content .text-content p');
+
+console.log(midText)
+
+document.addEventListener('wheel', zoom);
+
+function zoom(event) {
+    if (event.deltaY < 0) {
+      // Zoom in
+      scale *= event.deltaY * -1;
+    }
+    else {
+      // Zoom out
+      scale /= event.deltaY * 1;
+    }
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    midText.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
